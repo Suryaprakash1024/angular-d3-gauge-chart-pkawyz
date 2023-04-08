cd32b51c696b7b9b83e7a9da94fb61e8f516ee1b
Suryaprakash1024 <suryaprakash1024999@gmail.com>
2023-04-08 13:22:37 +0000

    all latest code

---
+++ src/app/app.component.ts
@@ -0,0 +1,213 @@
+import { Component } from '@angular/core';
+import * as d3 from "d3";
+
+// declare var d3: any;
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  gaugemap = {};
+  range = 240;
+  constructor() { }
+    ngOnInit() {
+    this.draw();
+  }
+
+  draw() {
+     var self = this;
+    var gauge = function (container :any, configuration :any) {
+    
+      var config= {
+        size: 300,
+        clipWidth: 300,
+        clipHeight: 300,
+        ringWidth: 60,
+        maxValue: 10,
+        transitionMs: 4000,
+        // size: 710,
+        // clipWidth: 200,
+        // clipHeight: 110,
+        ringInset: 20,
+        // ringWidth: 20,
+
+        pointerWidth: 10,
+        pointerTailLength: 5,
+        pointerHeadLengthPercent: 0.9,
+
+        minValue: 0,
+        // maxValue: 10,
+
+        minAngle: -90,
+        maxAngle: 90,
+
+        // transitionMs: 750,
+
+        majorTicks: 5,
+        labelFormat: d3.format('d'),
+        labelInset: 10,
+
+        arcColorFn: d3.interpolateHsl(d3.rgb('#e8e2ca'), d3.rgb('#3e6c0a'))
+      };
+      var range: number | undefined = undefined;
+      var r: string | number | undefined = undefined;
+      var pointerHeadLength: number | undefined = undefined;
+      var value = 0;
+
+      var svg: any | { append: (arg0: string) => { (): any; new(): any; attr: { (arg0: string, arg1: string): { (): any; new(): any; attr: { (arg0: string, arg1: string): any; new(): any; }; }; new(): any; }; data: { (arg0: number[][][]): { (): any; new(): any; attr: { (arg0: string, arg1: string): { (): any; new(): any; attr: { (arg0: string, arg1: string): any; new(): any; }; }; new(): any; }; }; new(): any; }; }; } | undefined = undefined;
+      var arc: any | undefined = undefined;
+      var scale: { (arg0: any): any; ticks: (arg0: number) => any; } | undefined = undefined;
+      var ticks = [0, 5, 7, 10];
+      var tickData : number[] = [];
+      var pointer: { transition: () => { (): any; new(): any; duration: { (arg0: number): { (): any; new(): any; ease: { (arg0: any): { (): any; new(): any; attr: { (arg0: string, arg1: string): void; new(): any; }; }; new(): any; }; }; new(): any; }; }; } | undefined = undefined;
+
+      var donut = d3.pie();
+      var gaugemap: any = {};
+
+      function deg2rad(deg : number) {
+        return deg * Math.PI / 180;
+      }
+
+      function newAngle(d : number) {
+        // var ratio = this.scale(d);
+        var newAngle = config.minAngle + (0.5 * 240);
+        return newAngle;
+      }
+
+      function configure(configuration : any={}) {
+        var prop = "";
+        //config = configuration;
+        // for (prop in configuration) {
+        //   config[prop] = configuration[prop];
+        // }
+
+        range = config.maxAngle - config.minAngle;
+        r = config.size / 2;
+        pointerHeadLength = Math.round(r * config.pointerHeadLengthPercent);
+
+        // a linear scale this.gaugemap maps domain values to a percent from 0..1
+        scale = d3.scaleLinear()
+          .range([0, 1])
+          .domain([config.minValue, config.maxValue]);
+
+        // ticks = scale.ticks(config.majorTicks);,
+        ticks = [0, 5, 7, 10];
+        tickData = [0.5, 0.4, 0.3];
+        // tickData = d3.range(config.majorTicks).map(function () { return 1 / config.majorTicks; });
+
+        arc = d3.arc()
+          .innerRadius(r - config.ringWidth - config.ringInset)
+          .outerRadius(r - config.ringInset)
+          .startAngle(function (d : any, i : number) {
+            var ratio = d * i;
+            return deg2rad(config.minAngle + (ratio * 240));
+          })
+          .endAngle(function (d:any, i:number) {
+            var ratio = d * (i + 1);
+            return deg2rad(config.minAngle + (ratio * 240));
+          });
+      }
+      gaugemap.configure = configure;
+
+      function centerTranslation() {
+        return 'translate(' + r + ',' + r + ')';
+      }
+
+      function isRendered() {
+        return (svg !== undefined);
+      }
+      gaugemap.isRendered = isRendered;
+
+      function render(newValue : any) {
+        svg = d3.select(container)
+          .append('svg:svg')
+          .attr('class', 'gauge')
+          .attr('width', config.clipWidth)
+          .attr('height', config.clipHeight);
+
+        var centerTx = centerTranslation();
+        let r = config.size / 2;
+        let pointerHeadLength = Math.round(r * config.pointerHeadLengthPercent);
+        if(svg)
+        var arcs = svg.append('g')
+          .attr('class', 'arc')
+          .attr('transform', centerTx);
+
+        arcs.selectAll('path')
+          .data(tickData)
+          .enter().append('path')
+          .attr('fill', function (d:any, i:number) {
+            return d3.interpolateHsl(d3.rgb('#e8e2ca'), d3.rgb('#3e6c0a'))(d * i);
+          })
+          .attr('d', arc);
+        if(svg)
+        var lg = svg.append('g')
+          .attr('class', 'label')
+          .attr('transform', centerTx);
+        lg.selectAll('text')
+          .data(ticks)
+          .enter().append('text')
+          .attr('transform', function (d:any) {
+            var ratio = 0.5;
+            var newAngle = config.minAngle + (ratio * 240);
+            return 'rotate(' + newAngle + ') translate(0,' + (config.labelInset - 0.5) + ')';
+          })
+          .text(config.labelFormat);
+          
+        var lineData = [[config.pointerWidth / 2, 0],
+        [0, -pointerHeadLength],
+        [-(config.pointerWidth / 2), 0],
+        [0, config.pointerTailLength],
+        [config.pointerWidth / 2, 0]];
+        var pointerLine = d3.line().curve(d3.curveLinear)
+        if(svg)
+        var pg = svg.append('g').data([lineData])
+          .attr('class', 'pointer')
+          .attr('transform', centerTx);
+
+        pointer = pg.append('path')
+          .attr('d', pointerLine/*function(d) { return pointerLine(d) +'Z';}*/)
+          .attr('transform', 'rotate(' + config.minAngle + ')');
+
+        update(newValue === undefined ? 0 : newValue);
+      }
+      gaugemap.render = render;
+      function update(newValue : any, newConfiguration? : any) {
+        if (newConfiguration !== undefined) {
+          // configure(newConfiguration);
+        }
+        var ratio = 0.5;
+        var newAngle = config.minAngle + (ratio * 240);
+        if(pointer)
+        pointer.transition()
+          .duration(config.transitionMs)
+          .ease(d3.easeElastic)
+          .attr('transform', 'rotate(' + newAngle + ')');
+      }
+      gaugemap.update = update;
+
+      configure(configuration);
+
+      return gaugemap;
+    };
+
+    var powerGauge = gauge('#power-gauge', {
+      size: 300,
+      clipWidth: 300,
+      clipHeight: 300,
+      ringWidth: 60,
+      maxValue: 10,
+      transitionMs: 4000,
+    });
+    powerGauge.render(6);
+
+  }
+}
+
+interface GaugeMap {
+  configure?: any;
+  isRendered?: any;
+  render?: any;
+  update?: any;
+}
