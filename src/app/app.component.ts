1f381591a07c70a84a12ac196b95f50edbaaac79
Suryaprakash1024 <suryaprakash1024999@gmail.com>
2023-04-08 13:41:36 +0000

    rm comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,28 +26,16 @@ export class AppComponent {
         ringWidth: 60,
         maxValue: 10,
         transitionMs: 4000,
-        // size: 710,
-        // clipWidth: 200,
-        // clipHeight: 110,
         ringInset: 20,
-        // ringWidth: 20,
-
         pointerWidth: 10,
         pointerTailLength: 5,
         pointerHeadLengthPercent: 0.9,
-
         minValue: 0,
-        // maxValue: 10,
-
-        minAngle: -90,
-        maxAngle: 90,
-
-        // transitionMs: 750,
-
+        minAngle: -120,
+        maxAngle: 120,
         majorTicks: 5,
         labelFormat: d3.format('d'),
         labelInset: 10,
-
         arcColorFn: d3.interpolateHsl(d3.rgb('#e8e2ca'), d3.rgb('#3e6c0a'))
       };
       var range: number | undefined = undefined;
@@ -57,7 +45,7 @@ export class AppComponent {
 
       var svg: any | { append: (arg0: string) => { (): any; new(): any; attr: { (arg0: string, arg1: string): { (): any; new(): any; attr: { (arg0: string, arg1: string): any; new(): any; }; }; new(): any; }; data: { (arg0: number[][][]): { (): any; new(): any; attr: { (arg0: string, arg1: string): { (): any; new(): any; attr: { (arg0: string, arg1: string): any; new(): any; }; }; new(): any; }; }; new(): any; }; }; } | undefined = undefined;
       var arc: any | undefined = undefined;
-      var scale: { (arg0: any): any; ticks: (arg0: number) => any; } | undefined = undefined;
+      var scale: any | { (arg0: any): any; ticks: (arg0: number) => any; } | undefined = undefined;
       var ticks = [0, 5, 7, 10];
       var tickData : number[] = [];
       var pointer: { transition: () => { (): any; new(): any; duration: { (arg0: number): { (): any; new(): any; ease: { (arg0: any): { (): any; new(): any; attr: { (arg0: string, arg1: string): void; new(): any; }; }; new(): any; }; }; new(): any; }; }; } | undefined = undefined;
@@ -77,10 +65,6 @@ export class AppComponent {
 
       function configure(configuration : any={}) {
         var prop = "";
-        //config = configuration;
-        // for (prop in configuration) {
-        //   config[prop] = configuration[prop];
-        // }
 
         range = config.maxAngle - config.minAngle;
         r = config.size / 2;
@@ -100,17 +84,35 @@ export class AppComponent {
           .innerRadius(r - config.ringWidth - config.ringInset)
           .outerRadius(r - config.ringInset)
           .startAngle(function (d : any, i : number) {
-            var ratio = d * i;
-            return deg2rad(config.minAngle + (ratio * 240));
+            if(i == 0)
+            {
+              return deg2rad(-120);
+            }
+            else if(i == 1){
+              return deg2rad((tickData[0] * 240)-120);
+            }
+            else if(i == 2){
+              return deg2rad(90 - d * 240);
+            }
+            return 0;
           })
           .endAngle(function (d:any, i:number) {
-            var ratio = d * (i + 1);
-            return deg2rad(config.minAngle + (ratio * 240));
+            if(i == 0){
+              return deg2rad((d *240)-120);
+              }
+              else if(i == 1){
+                return deg2rad(((tickData[0] * 240)-120) + d * 240);
+              }
+              else if(i == 2){
+                return deg2rad(120);
+              }
+              return 0;
           });
       }
       gaugemap.configure = configure;
 
       function centerTranslation() {
+        r = config.size / 2;
         return 'translate(' + r + ',' + r + ')';
       }
 
@@ -149,9 +151,9 @@ export class AppComponent {
           .data(ticks)
           .enter().append('text')
           .attr('transform', function (d:any) {
-            var ratio = 0.5;
+            var ratio = scale(d);
             var newAngle = config.minAngle + (ratio * 240);
-            return 'rotate(' + newAngle + ') translate(0,' + (config.labelInset - 0.5) + ')';
+            return 'rotate(' + newAngle + ') translate(0,' + (config.labelInset - r) + ')';
           })
           .text(config.labelFormat);
           
